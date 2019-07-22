c7beb9cfc2998eb584857987a4fed25bcd57eb39
why <why@163.com>
2019-07-22 13:18:56 +0800

    创建cat类

---
+++ weibo/weibo/cat.h
@@ -0,0 +1,13 @@
+//
+//  cat.h
+//  weibo
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Hello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cat : NSObject
+
+@end
---
+++ weibo/weibo/cat.m
@@ -0,0 +1,14 @@
+
+//
+//  cat.m
+//  weibo
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Hello. All rights reserved.
+//
+
+#import "cat.h"
+
+@implementation cat
+
+@end
