b795f7a2cc0eb9aebc7d33116410f423ac0382bc
why <why@163.com>
2019-07-22 13:15:19 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Hello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by hello on 2019/7/22.
+//  Copyright © 2019年 Hello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
